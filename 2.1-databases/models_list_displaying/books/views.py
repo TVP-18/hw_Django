eec92329b05010b06d732fd1dae6e3929c0c270b
Tatiana Popova <tatiana_mail@bk.ru>
2021-08-08 21:01:45 +0700

    Pagination is done

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from books.models  import Book
+from books.models import Book
 from datetime import datetime
+
 
 def index(request):
     return redirect('books')
@@ -17,15 +18,27 @@ def books_view(request):
 
 
 def books_date_view(request, year, month, day):
-    template = 'books/books_list.html'
+    template = 'books/books_date.html'
 
     books = Book.objects.filter(pub_date=datetime(year, month, day)).all()
 
+    # данные для пагинации
     date_next = Book.objects.filter(pub_date__gt=datetime(year, month, day)).order_by('pub_date').first()
+    date_preview = Book.objects.filter(pub_date__lt=datetime(year, month, day)).order_by('-pub_date').first()
 
     context = {
         'books': books,
-        'date_next': date_next,
+        'page_next': get_page(date_next),
+        'page_preview': get_page(date_preview),
     }
 
     return render(request, template, context)
+
+
+def get_page(date):
+    page = ''
+    if date is not None:
+        page = f'{date.pub_date.year}-{date.pub_date.month:02d}-{date.pub_date.day:02d}'
+    return page
+
+
