5e2bc134e92f2fd4006f1c972890fca96c12a8f4
Tatiana Popova <tatiana_mail@bk.ru>
2021-08-08 13:56:02 +0700

    database: create, set up and migration

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.TextField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.TextField()
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
