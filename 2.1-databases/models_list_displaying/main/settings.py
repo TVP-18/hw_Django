a54a5abd88321b2a908ec630d1846936391d3c00
Tatiana Popova <tatiana_mail@bk.ru>
2021-08-08 18:48:40 +0700

    set up database

--- 2.1-databases/models_list_displaying/main/settings.py
+++ 2.1-databases/models_list_displaying/main/settings.py
@@ -54,7 +54,9 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_models_list',
+        'NAME': 'netology_books',
+        'USER': 'admin_books',
+        'PASSWORD': 'books',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
