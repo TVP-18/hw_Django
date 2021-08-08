c484332406498a6db606312a1478ab3962948f4e
Tatiana Popova <tatiana_mail@bk.ru>
2021-08-08 14:33:52 +0700

    database: change type of fields - 'name', 'slug'

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Phone(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.TextField()
     release_date = models.DateField()
     lte_exists = models.BooleanField()
-    slug = models.TextField()
+    slug = models.SlugField()
 
     def __str__(self):
         return f"{self.id}: {self.name}"
