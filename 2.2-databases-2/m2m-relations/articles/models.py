bc3861773c860ba26edd1da13da0e27e8df55b1b
Tatiana Popova <tatiana_mail@bk.ru>
2021-08-15 12:33:48 +0700

    change models and create migration

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
 
+class Tag(models.Model):
+
+    name = models.CharField(max_length=50, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    scopes = models.ManyToManyField(Tag, through='Scope', through_fields=('article', 'tag'))
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +27,16 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.DO_NOTHING)
+    tag = models.ForeignKey(Tag, on_delete=models.DO_NOTHING, verbose_name='Раздел')
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+
+    def __str__(self):
+        return f'{self.tag.name} - {self.is_main}'
