6f2736988566b6737c85c3630a2ef5716374ee8d
Tatiana Popova <tatiana_mail@bk.ru>
2021-08-26 11:58:24 +0700

    Выполненно обязательное задание

--- 3.1-drf-intro/simple_crud/measurements/views.py
+++ 3.1-drf-intro/simple_crud/measurements/views.py
@@ -1,11 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 
+from .serializers import ProjectModelSerializer, MeasurementModelSerializer
+from .models import Project, Measurement
+
 
 class ProjectViewSet(ModelViewSet):
-    """ViewSet для проекта."""
-    # TODO: добавьте конфигурацию для объекта
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
 
 
 class MeasurementViewSet(ModelViewSet):
-    """ViewSet для измерения."""
-    # TODO: добавьте конфигурацию для измерения
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementModelSerializer
