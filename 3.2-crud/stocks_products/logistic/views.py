412128ffd46bfacfcae71d86f50338e832003049
Tatiana Popova <tatiana_mail@bk.ru>
2021-09-02 17:32:36 +0700

    Сделано:  поиск складов по id, title и description продукта

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,6 +1,8 @@
+from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -19,8 +21,16 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
 
-    # фильтрация
-    filter_backends = [DjangoFilterBackend]
-    filter_fields = ['products']
+    # поиск по id, title или description продукта
+    def get_queryset(self):
+        param = self.request.GET.get('products', None)
 
-
+        if param is None:
+            queryset = Stock.objects.all()
+        else:
+            if param.isdigit():
+                queryset = Stock.objects.filter(products=param)
+            else:
+                queryset = Stock.objects.filter(Q(products__title__icontains=param) |
+                                            Q(products__description__icontains=param))
+        return queryset
