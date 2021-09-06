d709ef6c9c646bee1d1e99f5d76512614a3ff9c9
Tatiana Popova <tatiana_mail@bk.ru>
2021-09-06 19:29:58 +0700

    Выполнено: "Обновлять и удалять объявление может только автор этого объявления"

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -5,6 +5,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
+from advertisements.permissions import IsOwner
 from advertisements.serializers import AdvertisementSerializer
 
 
@@ -14,11 +15,21 @@ class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
 
+    # фильтры по creator, status, create_at
     filter_backends = [DjangoFilterBackend]
     filterset_class = AdvertisementFilter
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        # if self.action in ["create", "update", "partial_update"]:
+        #     return [IsAuthenticated()]
+
+        # создать объявление может зарегистрированный пользователь
+        if self.action in ["create"]:
             return [IsAuthenticated()]
+
+        # обновить, удалить объявление только владелец
+        if self.action in ["update", "partial_update", "destroy"]:
+            return [IsOwner()]
+
         return []
